e7656c5900ecb3d805fbc49d3aa5842cce152d2e
Tom Suworof <rc1995king@gmail.com>
2024-06-12 17:30:09 +0300

    Fixed company model graph generator

--- src/main/java/com/salat/company_model/Experiment.java
+++ src/main/java/com/salat/company_model/Experiment.java
@@ -19,19 +19,36 @@ public record Experiment(
         Float resultProductPrice,
         Integer ticksPassed
 ) implements Entity {
+    /*
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> rdf:type <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/Experiment>
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> rdfs:label "Experiment 1"
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/run_number> 1
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/total_customers_number> 1
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1>  <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/salary> 1
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/product_costs> 1
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/starting_balance> 1
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/starting_revenue> 10000
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/result_revenue> 11070.268090757792
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/result_costs> 10994.6196763185
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/result_product_price> 1.1142705314999999
+<http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/runNumber/1> <http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/ticks_passed> 32123
+     */
+
     @Override
     public List<Triple> toTriples() {
         return List.of(
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), rdfsPrefix + "type", owlPrefix + "NamedIndividual"),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "run_number", runNumber()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "total_customers_number", totalCustomersNumber()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "salary", salary()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "product_costs", productCosts()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "starting_balance", startingBalance()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "result_revenue", resultRevenue()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "result_costs", resultCosts()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "result_product_price", resultProductPrice()),
-                createTriple(myOntologyPrefix + "experiment" + runNumber(), myOntologyPrefix + "ticks_passed", ticksPassed())
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), rdfPrefix + "type", myOntologyPrefix + "Experiment"),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), rdfsPrefix + "label", "Experiment " + runNumber()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "run_number", runNumber()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "total_customers_number", totalCustomersNumber()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "salary", salary()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "product_costs", productCosts()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "starting_balance", startingBalance()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "starting_revenue", startingRevenue()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "result_revenue", resultRevenue()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "result_costs", resultCosts()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "result_product_price", resultProductPrice()),
+                createTriple(myOntologyPrefix + "runNumber/" + runNumber(), myOntologyPrefix + "ticks_passed", ticksPassed())
         );
     }
 }
--- src/main/java/com/salat/company_model/TripleGenerator.java
+++ src/main/java/com/salat/company_model/TripleGenerator.java
@@ -6,8 +6,8 @@ import org.apache.jena.testing_framework.NodeCreateUtils;
 
 public class TripleGenerator {
     public static final String myOntologyPrefix = "http://www.semanticweb.org/rc199/ontologies/2024/5/company-model/";
-    public static final String rdfsPrefix = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-    public static final String owlPrefix = "http://www.w3.org/2002/07/owl#";
+    public static final String rdfsPrefix = "http://www.w3.org/2000/01/rdf-schema#";
+    public static final String rdfPrefix = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 
     public static Triple createTriple(String subject, String predicate, Object object) {
         return Triple.create(
